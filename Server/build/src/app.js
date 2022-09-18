aaaf873292a66e23c7669daa54c115495f450ae2
Eduardo <70926975+librotero@users.noreply.github.com>
2022-09-18 20:17:26 -0300

    Passport, UserSchema
    Creado el archivo passport en middlewares, además actualice el modelo de usuario y la encriptación de la contraseña

--- Server/build/src/app.js
+++ Server/build/src/app.js
@@ -5,5 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
 const express_1 = __importDefault(require("express"));
+//import passportmiddleware from './middlewares/passport/passport'
+require("./config/mongodb/db.js");
 const server = (0, express_1.default)();
 exports.server = server;
