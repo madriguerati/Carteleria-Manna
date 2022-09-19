701dbf969f2a2fa8af1403773286b61ce8e52f5b
Eduardo <70926975+librotero@users.noreply.github.com>
2022-09-19 13:03:13 -0300

    actualizacion con build

--- Server/build/src/app.js
+++ Server/build/src/app.js
@@ -5,6 +5,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
 const express_1 = __importDefault(require("express"));
+//import passport from 'passport'
+const index_1 = __importDefault(require("./Routes/index"));
+const passport_1 = __importDefault(require("passport"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const passport_2 = __importDefault(require("./middlewares/passport/passport"));
 require("../build/src/config/mongodb/db.js");
+const cors_1 = __importDefault(require("cors"));
 const server = (0, express_1.default)();
 exports.server = server;
+server.use(express_1.default.json());
+//cors config
+server.use(body_parser_1.default.urlencoded({ extended: true, limit: '50mb' }));
+server.use(body_parser_1.default.json({ limit: '50mb' }));
+server.use((0, cookie_parser_1.default)());
+server.use((0, morgan_1.default)('dev'));
+server.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+//passport
+server.use(passport_1.default.initialize());
+passport_1.default.use(passport_2.default);
+//routes
+server.use('/api', index_1.default);
+//cors
+server.use((0, cors_1.default)());
+server.use((err, _req, res, _next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send({ message });
+});
