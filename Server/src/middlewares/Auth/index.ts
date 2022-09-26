429436ab4516069ea1edfe8b3644fe56cdd1fbbc
Eduardo <70926975+librotero@users.noreply.github.com>
2022-09-26 15:26:19 -0300

    restricciones- insumos
    insumos habilitados solo a gerentes y administradores

--- Server/src/middlewares/Auth/index.ts
+++ Server/src/middlewares/Auth/index.ts
@@ -1,5 +1,9 @@
 import {verifyToken} from './verifyToken/index'
 import {isObrero} from './verifyObrero/index'
+import {isGerente} from './verifyGerente/index'
+import {isVendedor} from './verifyVendedor/index'
+import {isAdmin} from './verifyAdmin/index'
 
 
-export { verifyToken, isObrero};
+
+export { verifyToken, isObrero, isAdmin, isVendedor, isGerente};
