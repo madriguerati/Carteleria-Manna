7cdc97132334bc52e6a0d584a115c3f0c54bb5c3
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2022-09-27 23:45:29 -0300

    Update index.ts

--- Server/src/Routes/user/SignUp/index.ts
+++ Server/src/Routes/user/SignUp/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import createToken from '../../../Controllers/Token/createdToken'
 
 import User, { IUser } from '../../../Models/user';
 import Role from '../../../Models/roles';
@@ -25,7 +26,8 @@ try{
   }
    // Saving the User Object in Mongodb
    const savedUser = await newUser.save();
-   return res.status(201).json(newUser);
+   //return res.status(201).json(newUser);
+   return res.status(200).json({ token: createToken(newUser) });
 } catch (error){
   return res.status(500).json(error);
 }
