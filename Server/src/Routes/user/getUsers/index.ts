211f5a8377d5fc92f16a15f00b18795553c4b895
sky <ant_664@hotmail.com>
2022-10-04 22:37:16 -0500

    cambio en datos enviados del usuario

--- Server/src/Routes/user/getUsers/index.ts
+++ Server/src/Routes/user/getUsers/index.ts
@@ -4,7 +4,7 @@ import User from '../../../Models/user'
 
 const router = Router();
 
-router.get('/profile', async(req, res, next)=>{
+router.get('/profiles', async(req, res, next)=>{
     
     try{
         const users = await User.find() 
